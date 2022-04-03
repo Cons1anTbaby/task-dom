f5a52e222b90b4f89748ed5c10f42e722737c389
Константин <sakancev66@gmail.com>
2022-04-03 14:53:23 +0500

    Работа с DOM

--- src/DOM.js
+++ src/DOM.js
@@ -5,6 +5,11 @@
   Считаем, что всегда передается тег, допускающий вставку текста в качестве своего содержимого (P, DIV, I и пр.).
 */
 export function appendToBody(tag, content, count) {
+    const htmlTag = [];
+    for (let i = 0; i < count; i++) {
+        htmlTag.push(`<${tag}>${content}</${tag}>`);
+    }
+    document.body.innerHTML = htmlTag.join('');
 }
 
 /*
@@ -15,6 +20,26 @@ export function appendToBody(tag, content, count) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function generateTree(childrenCount, level) {
+    return generateNode(childrenCount, 1, level);
+}
+
+function generateNode(childrenCount, level, maxLevel) {
+    let newNode;
+    if (maxLevel > level) {
+        newNode = document.createElement('div');
+        newNode.className = `item_${level}`;
+        level++;
+        for (let i = 0; i < childrenCount; i++) {
+            newNode.append(generateNode(childrenCount, level, maxLevel));
+        }
+    } else {
+        for (let i = 0; i < childrenCount; i++) {
+            newNode = document.createElement('div');
+            newNode.className = `item_${level}`;
+        }
+        return newNode;
+    }
+    return newNode;
 }
 
 /*
@@ -26,4 +51,18 @@ export function generateTree(childrenCount, level) {
   Сформированное дерево верните в качестве результата работы функции.
 */
 export function replaceNodes() {
+    let htmlTag = generateTree(2, 3);
+    let item2Elems = htmlTag.getElementsByClassName('item_2');
+    for (let i = 0; i < item2Elems.length; i++) {
+        let elementNew = document.createElement('section');
+        elementNew.className = 'item_2';
+        elementNew.innerHTML = item2Elems[i].innerHTML;
+        item2Elems[i].replaceWith(elementNew);
+    }
+    //     elementNew.innerHTML = item2Elems[i].innerHTML;
+
+    //     item2Elems[i].parentNode.insertBefore(elementNew, item2Elems[i]);
+    //     item2Elems[i].parentNode.removeChild(item2Elems[i]);
+    // }
+    return htmlTag;
 }
